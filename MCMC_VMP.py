76329f66bd1fafe4a100ece3aa41841776d3561d
icruzso2403 <icruzso2403@gmail.com>
2024-11-12 12:59:12 -0300

    MCMC_VMP

--- MCMC_VMP.py
+++ MCMC_VMP.py
@@ -1,128 +1,183 @@
 
 import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+from sklearn.mixture import GaussianMixture
 
 class MCMC_VMP:
 
     def __init__(
             
-        self, 
+        self,
         
-        Dados: np.ndarray,
+        Dados: np.ndarray, Categorias: int,
 
-        mu_0: np.ndarray, sigma_0: np.ndarray,
+        nu_0: float, chi_0: np.ndarray,
 
-        alpha_0: np.ndarray, beta_0: np.ndarray
+        Distribuicao, 
+        
+        Amostra: int = 10, Descartadas:int = 20
 
     ) -> None:
-
+        
+        # Dados observados
+        
         self.X = Dados
+
+        # Número de observações
 
         self.N = self.X.shape[0]
 
-        self.mu_0 = mu_0
+        # Número de categorias latentes
 
-        self.sigma_0 = sigma_0
+        self.K = Categorias
 
-        self.m = 0
+        # Parâmetros das categorias latentes
 
-        self.sigma = 0
+        self.r = np.zeros(shape = (self.N, self.K))
 
-        self.mu = 0
+        self.N_barra = np.zeros(shape = (self.K))
 
-        self.mu_quadrado = 0
+        # Parâmetros a priori
 
-        self.alpha_0 = alpha_0
+        self.nu_0 = nu_0
 
-        self.beta_0 = beta_0
+        self.chi_0 = chi_0
 
-        self.alpha = 0
+        # Dimensão dos parâmetros
 
-        self.beta = 0
+        self.D = self.chi_0.shape[0]
 
-        self.gamma = 0
+        # Estatística suficiente conjunta
 
-        self.ln_gamma = 0
+        self.u_barra = np.zeros(shape = (self.K, self.N, self.D))
 
-    def atualiza_mu(self) -> None:
+        # Parâmetros a priori a posteriori
 
-        self.mu_quadrado = np.random.normal(loc = self.m, scale = 1/self.sigma, size = 1000)
+        self.nu = np.zeros(shape = (self.K))
 
-        self.mu = self.mu_quadrado.mean()
+        self.chi = np.zeros(shape = (self.K, self.D))
 
-    def atualiza_mu_quadrado(self) -> None:
+        # Número de amostras consideradas
 
-        self.mu_quadrado = np.random.normal(loc = self.m, scale = 1/self.sigma, size = 1000)
+        self.M = Amostra
 
-        self.mu_quadrado = self.mu_quadrado**2
+        # Número de amostras descartadas
 
-        self.mu_quadrado = self.mu_quadrado.mean()
+        self.B = Descartadas
 
-    def atualiza_gamma(self) -> None:
+        # Amostras do parâmetro natural
 
-        self.gamma = np.random.gamma(shape = self.alpha, scale = 1/self.beta, size = 1000)
+        self.tau = np.zeros(shape = (self.K, self.M, self.D))
 
-        self.gamma = self.gamma.mean()
+        # Estimador de bayes
 
-    def atualiza_ln_gamma(self) -> None:
+        self.eta = np.zeros(shape = (self.K, self.D))
 
-        self.ln_gamma = np.random.gamma(shape = self.alpha, scale = 1/self.beta, size = 1000)
+        # Classe de distribuições da família exponencial
 
-        self.ln_gamma = np.log(self.ln_gamma)
-
-        self.ln_gamma = self.ln_gamma.mean()
-
-    def atualiza_sigma(self) -> None:
-
-        self.sigma = (self.sigma_0 + self.N*self.gamma)
-
-    def atualiza_m(self) -> None:
-
-        self.m = self.sigma*self.mu_0 + self.gamma*self.X.sum()
-
-        self.m = self.m/self.sigma
-
-    def atualiza_beta(self) -> None:
-
-        self.beta = self.beta_0
-
-        self.beta += (self.X**2).sum()/2
-
-        self.beta -= self.X.sum()*self.mu
-
-        self.beta += self.N*self.mu_quadrado/2
-
-    def atualiza_alpha(self) -> None:
-
-        self.alpha = self.alpha_0 + self.N/2
+        self.F = Distribuicao
 
     def inicializa_modelo(self) -> None:
 
-        self.mu = np.random.uniform(low = self.X.min(), high = self.X.max())
+        self.r = GaussianMixture(
 
-        self.mu_quadrado = self.mu**2
+            n_components = self.K, max_iter = 0, 
+            
+            init_params = 'k-means++'
 
-        self.gamma = np.random.uniform(low = 0, high = self.X.max())
+        ).fit(self.X).predict_proba(self.X)
 
-        self.ln_gamma = np.log(self.gamma)
+    def log_q(self, k:int, eta: np.ndarray) -> None:
 
-    def passa_menssagens(self, max: int = 100) -> None:
+        log_q = np.dot(eta, self.chi[k])
+
+        log_q -= self.nu[k]*self.F.A(eta = eta)
+
+        return log_q
+    
+    def atualiza_u_barra(self) -> None:
+
+        for k in range(self.K):
+
+            for n in range(self.N):
+
+                self.u_barra[k][n] = self.F.U(self.X[n])
+
+                self.u_barra[k][n] *= self.r[n][k]
+
+    def atualiza_chi(self) -> None:
+
+        self.chi = self.u_barra.sum(axis = 1)
+
+        self.chi += self.chi_0.reshape((1, self.D))
+
+    def atualiza_N_barra(self) -> None:
+
+        self.N_barra = self.r.sum(axis = 0)
+
+    def atualiza_nu(self) -> None:
+
+        self.nu = self.nu_0 + self.N_barra
+
+    def atualiza_tau(self) -> None:
+
+        for k in range(self.K):
+
+            self.tau[k] = tfp.mcmc.sample_chain(
+            
+                num_results = self.M, num_burnin_steps = self.B,
+
+                current_state = np.ones(shape = self.D),
+
+                kernel = tfp.mcmc.RandomWalkMetropolis(
+
+                    target_log_prob_fn = lambda eta: self.log_q(k = k, eta = eta)
+
+                ), trace_fn = None
+
+            )
+
+    def atualiza_eta(self) -> None:
+
+        self.eta = self.tau.mean(axis = 1)
+
+    def atualiza_r(self) -> None:
+
+        for k in range(self.K):
+
+            for n in range(self.N):
+
+                self.r[n][k] = self.eta[k] @ self.F.U(x = self.X[n])
+
+                self.r[n][k] -= self.F.A(eta = self.eta[k])
+
+        self.r = np.exp(self.r)
+
+        self.r /= self.r.sum(axis = 1).reshape((self.N, 1))
+
+    def atualiza_modelo(self) -> None:
+
+        self.atualiza_u_barra()
+
+        self.atualiza_chi()
+
+        self.atualiza_N_barra()
+
+        self.atualiza_nu()
+
+        self.atualiza_tau()
+
+        self.atualiza_eta()
+
+        self.atualiza_r()
+
+    def estima_modelo(self, num: int = 30) -> None:
 
         self.inicializa_modelo()
 
-        for i in range(max):
+        self.atualiza_modelo()
 
-            self.atualiza_sigma()
+        for i in range(num):
 
-            self.atualiza_m()
-
-            self.atualiza_mu()
-
-            self.atualiza_mu_quadrado()
-
-            self.atualiza_alpha()
-
-            self.atualiza_beta()
-
-            self.atualiza_gamma()
-
-            self.atualiza_ln_gamma()
+            self.atualiza_modelo()
